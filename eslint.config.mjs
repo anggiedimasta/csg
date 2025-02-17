bf150441731db09bb6e03d9e9e5a1517201d2caa
anggiedimasta <anggiedimasta@gmail.com>
2025-02-17 23:37:08 +0700

    chore(config): modified lint and added tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,12 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
 	rules: {
-		'@stylistic/arrow-parens': [2, 'always'],
-		'@stylistic/comma-dangle': [2, 'always-multiline'],
-		'@stylistic/indent': [2, 'tab', { SwitchCase: 1 }],
-		'@stylistic/no-mixed-spaces-and-tabs': 2,
-		'@stylistic/quotes': [2, 'single', { avoidEscape: true }],
-		'@stylistic/semi': [2, 'never'],
-		'@stylistic/space-before-function-paren': [2, 'never'],
-	},
+		'arrow-parens': [2, 'always'],
+		'comma-dangle': [2, 'never'],
+		'indent': [2, 'tab', { SwitchCase: 1 }],
+		'no-mixed-spaces-and-tabs': 2,
+		'quotes': [2, 'single', { avoidEscape: true }],
+		'semi': [2, 'never'],
+		'space-before-function-paren': [2, 'never']
+	}
 })
